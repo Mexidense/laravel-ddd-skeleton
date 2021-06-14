ea6dfe9dcab8061da0b2db456e7753f8640a1ea8
Salvador Briones <salvador.briones@selectra.info>
2021-06-14 14:49:49 +0200

    feat: add Console command kernel to Core infrastructure bounded context

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -33,7 +33,7 @@ $app->singleton(
 
 $app->singleton(
     Illuminate\Contracts\Console\Kernel::class,
-    App\Console\Kernel::class
+    App\Core\Infrastructure\Console\Kernel::class
 );
 
 $app->singleton(

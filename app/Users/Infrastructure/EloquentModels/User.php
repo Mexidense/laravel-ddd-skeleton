5a0bbe658b6835e90c16108ea522fd86dbcf91c8
Salvador Briones <salvador.briones@selectra.info>
2021-06-14 16:24:13 +0200

    feat: move User model to User bounded context

--- app/Users/Infrastructure/EloquentModels/User.php
+++ app/Users/Infrastructure/EloquentModels/User.php
@@ -1,42 +1,32 @@
 <?php
 
-namespace App\Models;
+declare(strict_types=1);
+
+namespace App\Users\Infrastructure\EloquentModels;
 
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-class User extends Authenticatable
+final class User extends Authenticatable
 {
     use HasFactory, Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
+    /** @var array */
     protected $fillable = [
         'name',
         'email',
         'password',
     ];
 
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
+    /** @var array */
     protected $hidden = [
         'password',
         'remember_token',
     ];
 
-    /**
-     * The attributes that should be cast to native types.
-     *
-     * @var array
-     */
+    /**  @var array */
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
